c8dac313127d5636f14d72ea78fc5f116060bfb9
yaoxt3 <759741745@qq.com>
2018-06-03 22:03:28 +0800

    2018-6-3-1.0

---
+++ cmake-build-debug/CMakeFiles/lidar.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lidar.dir/src/lidar_main.cpp.o"
+  "devel/lib/lidar/lidar.pdb"
+  "devel/lib/lidar/lidar"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lidar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/yxt/workspace/lidarnode
+# Build directory: /home/yxt/workspace/lidarnode/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
